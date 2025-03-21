493e653699f34f17d8ce2bfe572e4f4e475317d1
Ayumu Tamiya <167746363+tamiyaayumu@users.noreply.github.com>
2025-03-21 14:45:21 +0900

    NomadTaskを追加

---
+++ assets/basic/NomadTasks.js
@@ -0,0 +1,114 @@
+// @ts-check
+
+/**
+ * @typedef {'TaskFlow' | 'SubTask'} TaskType
+ * @typedef {'Waiting' | 'Completed'} TaskStatus
+ */
+
+/**
+ * @typedef {Object} ToDo
+ * @property {string} Method
+ * @property {any} [Data]
+ */
+
+/**
+ * @typedef {Object} Task
+ * @property {boolean} [Hidden]
+ * @property {string} [Alias]
+ * @property {TaskType} Type
+ * @property {string} TaskID
+ * @property {ToDo} [ToDo]
+ * @property {ToDo} [Check]
+ * @property {string | null} Dependencies
+ * @property {TaskStatus} Status
+ * @property {string[]} [Requirements]
+ * @property {Task[]} [Flow]
+ */
+
+/** @type {Task} */
+export const NomadTasks = {
+    Type: "TaskFlow",
+    Alias: "Nomad Tasks",
+    TaskID: "NomadTasks",
+    Dependencies: "null",
+    Status: "Waiting",
+    Flow: [
+        {
+            Type: "SubTask",
+            Alias: "Introduction",
+            TaskID: "Introduction",
+            ToDo: {
+                Method: "Replace",
+                Data: {
+                    Target:"Introduction",
+                    Task:"introduction",
+                    ReplaceData: {}
+                }
+            },
+            Check: {
+                Method: "null"
+            },
+            Dependencies: "null",
+            Status: "Waiting"
+        },
+        {
+            Type: "SubTask",
+            Alias: "TransferToEarbuzz",
+            TaskID: "TransferToEarbuzz",
+            ToDo: {
+                Method: "Replace",
+                Data: {
+                    Target:"TransferToEarbuzz",
+                    Task:"MigrateBrowserToEarbuzz",
+                    ReplaceData: {}
+                }
+            },
+            Check: {
+                Method: "null"
+            },
+            Dependencies: "null",
+            Status: "Waiting"
+        },
+        {
+            Type: "SubTask",
+            Alias: "esspressodemo",
+            TaskID: "espressodemo",
+            ToDo: {
+                Method: "Replace",
+                Data: {
+                    Target:"espressodemo",
+                    Task:"EspressoDemo",
+                    ReplaceData: {}
+                }
+            },
+            Check: {
+                Method: "null"
+            },
+            Dependencies: "null",
+            Status: "Waiting"
+        },
+        {
+            Type: "SubTask",
+            Alias: "TransfertoCommu",
+            TaskID: "Transfertocommu",
+            ToDo: {
+                Method: "TaskQueue",
+                Data: {
+                    Target:"TransfertoCommu",
+                    Task:"Task_TransferBasic",
+                    ReplaceData: {
+                        newDeviceName: "コミュ",
+                        newClientID: "{{コミュ_ID}}"
+                    }
+                }
+            },
+            Check: {
+                Method: "null"
+            },
+            Dependencies: "null",
+            Status: "Waiting"
+        },
+
+        
+    ]
+};
